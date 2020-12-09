c99207217c8fcda81c754cee4913aa29ac8ebcc8
Dirk Lemmermann <dlemmermann@gmail.com>
2020-12-09 15:58:28 +0100

    Added javafx maven plugin to run the app. Changed target java version from 15 to 11.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,16 +1,9 @@
-open module com.sidelinesports.coachfx {
+open module com.dlsc.opencv {
 
     requires java.prefs;
-    requires com.google.gson;
     requires javafx.swing;
-    requires org.controlsfx.controls;
-    requires fr.brouillard.oss.cssfx;
-    requires com.dlsc.unitfx;
     requires javafx.controls;
     requires opencv;
-    requires org.apache.commons.lang3;
-    requires java.desktop;
-    requires javafx.fxml;
 
     exports com.dlsc.opencv;
 }
